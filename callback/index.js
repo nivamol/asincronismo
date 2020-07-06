1121a7598d9849dd406dc5d3180925b1486b3ff1
nivamol <nicolas05valencia@gmail.com>
2020-07-06 15:00:00 -0500

    callbacks con setTimeOut

--- callback/index.js
+++ callback/index.js
@@ -7,3 +7,20 @@ function calc(num1, num2, callback) {
 }
 
 console.log(calc(2,2, suma));
+
+
+// ejemplo con tiempos. 
+
+function date(callback) {
+   console.log(new Date); // aquí accedemos a la fecha actual. 
+   setTimeout(function() {
+    let date = new Date;
+    callback(date) 
+   }, 3000)
+}
+
+function printDate(dateNow) {
+  console.log(dateNow);
+}
+
+date(printDate);
